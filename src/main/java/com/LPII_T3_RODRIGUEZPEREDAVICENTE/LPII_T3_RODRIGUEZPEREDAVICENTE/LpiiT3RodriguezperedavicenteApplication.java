94b786a7112d2c7b5c6404e4e288261d2dc227b4
TheRealVarpus <varp2006@gmail.com>
2025-02-19 23:18:40 -0500

    LPII_T3_RODRIGUEZPEREDAVICENTE-V1.0

---
+++ src/main/java/com/LPII_T3_RODRIGUEZPEREDAVICENTE/LPII_T3_RODRIGUEZPEREDAVICENTE/LpiiT3RodriguezperedavicenteApplication.java
@@ -0,0 +1,13 @@
+package com.LPII_T3_RODRIGUEZPEREDAVICENTE.LPII_T3_RODRIGUEZPEREDAVICENTE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiT3RodriguezperedavicenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiT3RodriguezperedavicenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LPII_T3_RODRIGUEZPEREDAVICENTE/LPII_T3_RODRIGUEZPEREDAVICENTE/LpiiT3RodriguezperedavicenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LPII_T3_RODRIGUEZPEREDAVICENTE.LPII_T3_RODRIGUEZPEREDAVICENTE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpiiT3RodriguezperedavicenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
